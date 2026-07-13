92f0af11c633fddf91a1856e9a37b7efd984b8ad
Jessica Kiroff <jesskiroff@gmail.com>
2026-07-12 20:38:09 -0400

    add equal valued dictionaries

--- pointers.py
+++ pointers.py
@@ -1,9 +1,34 @@
-num1 = 11
-num2 = num1
+# num1 = 11
+# num2 = num1
 
-print("Before num2 value is updated:")
-print("num1:", num1)
-print("num2:", num2)
+# print("Before num2 value is updated:")
+# print("num1:", num1)
+# print("num2:", num2)
 
-print("\nnum1 points to:", id(num1))
-print("num2 points to:", id(num2))
+# print("\nnum1 points to:", id(num1))
+# print("num2 points to:", id(num2))
+
+# num2 = 22
+
+# print("\nAfter num2 value is updated:")
+# print("num1:", num1)
+# print("num2:", num2)
+
+# print("\nnum1 points to:", id(num1))
+# print("num2 points to:", id(num2))
+
+# integers consistently point to the same place in memory. also dictionaries
+
+
+dict1 = {
+    'value': 11
+}
+
+dict2 = dict1
+
+print("Before value is updated:")
+print("dict1=", dict1)
+print("dict2=", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
